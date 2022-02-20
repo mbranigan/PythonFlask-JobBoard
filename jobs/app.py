c87d9329b8df0725a2848ba7dafd9c3aa4480ba0
Maeve Branigan <maevebranigan@gmail.com>
2022-02-20 18:53:22 +0000

    Completed module

---
+++ jobs/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+@app.route('/jobs', methods=['GET'])
+def jobs():
+    return render_template('index.html')
